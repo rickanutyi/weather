20bbd6b36471953c6cdc5b85974e39ade788f824
CH/Erlan <EChynybaev>
2023-09-30 20:48:12 +0600

    fix temp type switcher

--- src/components/TampTypeSwitcher.vue
+++ src/components/TampTypeSwitcher.vue
@@ -1,7 +1,7 @@
 <template>
     <q-select
         v-model="model"
-        :options="localeOptions"
+        :options="options"
         dense
         borderless
         emit-value
@@ -18,7 +18,7 @@ import { useI18n } from 'vue-i18n';
 
 export default {
     setup() {
-        const { t } = useI18n();
+        const { t, locale } = useI18n();
         const tempStore = useTempStore();
 
         const model = ref(tempStore.getTempType);
@@ -26,12 +26,20 @@ export default {
         watch(model, () => {
             tempStore.setTempType(model.value);
         });
+
+        const options = ref([
+            { value: 'C', label: t('celsius') },
+            { value: 'F', label: t('fahrenheit') },
+        ]);
+        watch(locale, () => {
+            //по какой то причине options не реактивен для локализации
+            options.value[0].label = t('celsius');
+            options.value[1].label = t('fahrenheit');
+        });
+
         return {
             model,
-            localeOptions: [
-                { value: 'C', label: t('celsius') },
-                { value: 'F', label: t('fahrenheit') },
-            ],
+            options,
         };
     },
 };
